e79cddb81b2e019d1becdb73cec4dcbf8f48c508
abhishekkant14 <Abhishekkant051@gmail.com>
2026-09-11 15:54:11 +0530

    Crud_Api

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -53,3 +53,134 @@ func CreateUser(db *sql.DB) gin.HandlerFunc {
 		})
 	}
 }
+func GetUsers(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		rows, err := db.Query("SELECT id, name, email, age FROM users")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		defer rows.Close()
+
+		var users []models.User
+
+		for rows.Next() {
+
+			var user models.User
+
+			err := rows.Scan(
+				&user.ID,
+				&user.Name,
+				&user.Email,
+				&user.Phone,
+			)
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": err.Error(),
+				})
+				return
+			}
+
+			users = append(users, user)
+		}
+
+		if err := rows.Err(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"users": users,
+		})
+	}
+}
+
+func UpdateUser(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request body",
+			})
+			return
+		}
+		result, err := db.Exec(
+			"UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?",
+			user.Name,
+			user.Email,
+			user.Phone,
+			id,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if rowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "User not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "User updated successfully",
+		})
+	}
+}
+func DeleteUser(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		id := c.Param("id")
+
+		result, err := db.Exec(
+			"DELETE FROM users WHERE id = ?",
+			id,
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if rowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "User not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "User deleted successfully",
+		})
+	}
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 
 	"user_api/controllers"
 
@@ -13,6 +14,7 @@ import (
 
 func main() {
 
+	// MySQL connection
 	dsn := "root:Abhishek@123456@tcp(127.0.0.1:3306)/user_db"
 
 	db, err := sql.Open("mysql", dsn)
@@ -21,16 +23,62 @@ func main() {
 	}
 	defer db.Close()
 
+	// Check MySQL connection
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("MySQL dB sucessfully Connected")
+	fmt.Println("MySQL dB successfully Connected")
 
+	// Create Gin router
 	router := gin.Default()
 
+	// CREATE user
 	router.POST("/users", controllers.CreateUser(db))
 
+	// GET all users
+	router.GET("/users", controllers.GetUsers(db))
+
+	router.PUT("/users/:id", controllers.UpdateUser(db))
+
+	// Start server
 	router.Run(":8080")
+}
+func DeleteUser(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		id := c.Param("id")
+
+		result, err := db.Exec(
+			"DELETE FROM users WHERE id = ?",
+			id,
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if rowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "User not found",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "User deleted successfully",
+		})
+	}
 }
